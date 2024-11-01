a39185f1275017e8b540c9e7f7583db14a6090b8
LuisVynicius <luismevy@gmail.com>
2024-11-01 09:00:52 -0300

    Created User entity

---
+++ src/main/java/com/mevy/stories/entities/User.java
@@ -0,0 +1,43 @@
+package com.mevy.stories.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Data;
+
+@Entity
+@Table(name = "tb_user")
+@Data
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(
+        updatable = false
+    )
+    private Long id;
+
+    @Column(
+        length = 15,
+        nullable = false
+    )
+    private String username;
+
+    @Column(
+        length = 100,
+        nullable = false,
+        unique = true
+    )
+    private String email;
+
+    @Column(
+        nullable = false
+    )
+    private String password;
+
+}
