cf7560e802e128922aab6eadc753ecafc1bac3eb
LuisVynicius <luismevy@gmail.com>
2024-12-28 12:22:06 -0300

    fix: Chapter

--- src/main/java/com/mevy/stories/repositories/ChapterRepositorie.java
+++ src/main/java/com/mevy/stories/repositories/ChapterRepositorie.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.mevy.stories.entities.Chapter;
 
-public interface ChapterRepositories extends JpaRepository<Chapter, Long> {
+public interface ChapterRepositorie extends JpaRepository<Chapter, Long> {
     
 }
---
+++ src/main/java/com/mevy/stories/services/ChapterService.java
@@ -0,0 +1,15 @@
+package com.mevy.stories.services;
+
+import org.springframework.stereotype.Service;
+
+import com.mevy.stories.repositories.ChapterRepositorie;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class ChapterService {
+    
+    private ChapterRepositorie ChapterRepositorie;
+
+}
