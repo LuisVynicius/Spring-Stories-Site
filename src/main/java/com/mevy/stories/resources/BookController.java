933521650453bd3b55d21581e710ebe330d943ae
LuisVynicius <luismevy@gmail.com>
2024-12-28 11:52:32 -0300

    Add: h2 database configurations

--- src/main/java/com/mevy/stories/resources/BookController.java
+++ src/main/java/com/mevy/stories/resources/BookController.java
@@ -1,9 +1,14 @@
 package com.mevy.stories.resources;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.mevy.stories.services.BookService;
 
 import lombok.AllArgsConstructor;
 
+@RestController
+@RequestMapping("/book")
 @AllArgsConstructor
 public class BookController {
     
