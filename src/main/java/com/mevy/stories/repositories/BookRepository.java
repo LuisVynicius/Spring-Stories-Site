7ea2384f300d2cbe8509732d83dc753863d16ad7
LuisVynicius <luismevy@gmail.com>
2024-12-28 11:51:38 -0300

    Fix: Repositories

--- src/main/java/com/mevy/stories/repositories/BookRepository.java
+++ src/main/java/com/mevy/stories/repositories/BookRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.mevy.stories.entities.Book;
 
-public interface BookRepository extends JpaRepository<Long, Book>{
+public interface BookRepository extends JpaRepository<Book, Long>{
     
 }
--- src/main/java/com/mevy/stories/repositories/CategoryRepository.java
+++ src/main/java/com/mevy/stories/repositories/CategoryRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.mevy.stories.entities.Category;
 
-public interface CategoryRepository extends JpaRepository<Long, Category> {
+public interface CategoryRepository extends JpaRepository<Category, Long> {
     
 }
