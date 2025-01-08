4f46585535cd16d15ed6b9c443354d85babb5ded
LuisVynicius <luismevy@gmail.com>
2025-01-08 15:18:39 -0300

    Add: Swagger

--- src/main/java/com/mevy/stories/StoriesApplication.java
+++ src/main/java/com/mevy/stories/StoriesApplication.java
@@ -3,7 +3,10 @@ package com.mevy.stories;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
 @SpringBootApplication
+@OpenAPIDefinition
 public class StoriesApplication {
 
 	public static void main(String[] args) {
