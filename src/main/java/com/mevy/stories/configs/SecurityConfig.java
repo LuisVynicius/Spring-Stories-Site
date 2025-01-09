b334166ee9cb5762a04f9a5f723282efc444a504
LuisVynicius <luismevy@gmail.com>
2025-01-09 08:39:27 -0300

    Add: SecurityConfig

---
+++ src/main/java/com/mevy/stories/configs/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.mevy.stories.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    
+    private static final String[] PUBLIC_MATCHERS_POST = {
+        "/login",
+        "/register"
+    };
+
+    private static final String[] PUBLIC_MATCHERS_GET = {
+        "/book/recents",
+        "/book/name/**"
+    };
+
+    public SecurityFilterChain SecurityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                    .csrf(csrf -> csrf.disable())
+                    .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
+                        .requestMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+                        .anyRequest().authenticated()
+                    )
+                    .build();
+    }
+
+    @Bean
+    BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
