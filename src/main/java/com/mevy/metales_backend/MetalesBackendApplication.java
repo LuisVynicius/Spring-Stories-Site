a82d8852e2f860fdef3e2688cf3393635fb13e21
LuisVynicius <luismevy@gmail.com>
2025-04-07 16:41:00 -0300

    Add: WebConfig

--- src/main/java/com/mevy/metales_backend/MetalesBackendApplication.java
+++ src/main/java/com/mevy/metales_backend/MetalesBackendApplication.java
@@ -2,12 +2,14 @@ package com.mevy.metales_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MetalesBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MetalesBackendApplication.class, args);
 	}
-
+	
 }
