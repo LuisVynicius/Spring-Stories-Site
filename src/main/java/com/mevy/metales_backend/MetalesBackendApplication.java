4fc1b27d6a785ce2a0f12ad845ecc29ddf56809a
LuisVynicius <luismevy@gmail.com>
2025-04-07 16:23:46 -0300

    Backend full reset

---
+++ src/main/java/com/mevy/metales_backend/MetalesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mevy.metales_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetalesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetalesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mevy/metales_backend/MetalesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mevy.metales_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetalesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
